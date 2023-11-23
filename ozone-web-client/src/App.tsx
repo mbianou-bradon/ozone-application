5fbd2d0d65b0a8c76d5fef8b58731ff9d5dbc6a5
mbianou-bradon <mbianoubradon2000@gmail.com>
2023-11-23 08:59:16 +0100

    fix: adjusted rendering method

--- ozone-web-client/src/App.tsx
+++ ozone-web-client/src/App.tsx
@@ -1,33 +1,31 @@
 import React, { useState } from "react";
-import logo from "./logo.svg";
-import "./App.css";
 import TextInput from "./components/Input/TextInput/TextInput";
 import Button from "./components/Button/Button";
-import SignUp from "./page/auth/sign-up/SignUp";
 import { DocumentSigning } from "./components";
 
 function App() {
   /** State Management */
   const [value, setValue] = useState<string>("");
   return (
-    <div className="px-4 flex items-center flex-col justify-center h-screen">
-      <TextInput
-        value={value}
-        setValue={setValue}
-        label={"First Name"}
-        placeholder={"Input Your Brand Name"}
-        required={true}
-        inputType={"text"}
-      />
+    // <div className="px-4 flex items-center flex-col justify-center h-screen">
+    //   <TextInput
+    //     value={value}
+    //     setValue={setValue}
+    //     label={"First Name"}
+    //     placeholder={"Input Your Brand Name"}
+    //     required={true}
+    //     inputType={"text"}
+    //   />
 
-      <div>
-        <Button text="Next" type="bordered" />
-      </div>
+    //   <div>
+    //     <Button text="Next" type="bordered" />
+    //   </div>
 
-      <div className="mt-10">
-        <DocumentSigning />
-      </div>
-    </div>
+    //   <div className="mt-10">
+    //     <DocumentSigning />
+    //   </div>
+    // </div>
+    <div></div>
   );
 }
 
--- ozone-web-client/src/components/DocumentSigning/DocumentSigning.tsx
+++ ozone-web-client/src/components/DocumentSigning/DocumentSigning.tsx
@@ -4,8 +4,8 @@ import { Button } from "..";
 export default function DocumentSigning() {
   return (
     <div>
-      <div className="py-2 px-2 w-full flex items-center justify-between">
-        <p>Electronically sign the agreement(s)</p>
+      <div className="py-2 px-2 w-full flex items-center justify-between border border-neutral-gray rounded-md">
+        <p className="text-slate-800">Electronically sign the agreement(s)</p>
         <p>&times;</p>
       </div>
       <Button rightIcon type="filled" />
